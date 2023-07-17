1ac151e66311d8a7feb730eca79e239e85a5a9d0
Himanshu Tak <himanshu05.tak@gmail.com>
2023-07-17 15:21:29 +0200

    - Added MainMenu.unity - Added LoadingScene.unity - Implemented Loading Logic - Implemented Main menu Logic

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool _isFirstUpdate;
+    void Update()
+    {
+        if (_isFirstUpdate) return;
+        _isFirstUpdate = true;
+        Loader.LoaderCallback();
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button playBtn;
+        [SerializeField] private Button quitBtn;
+
+        private void Awake()
+        {
+            playBtn.onClick.AddListener(() => { Loader.Load(Loader.Scene.GamePlay); });
+            quitBtn.onClick.AddListener(Application.Quit);
+        }
+    }
+}
